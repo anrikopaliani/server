2395f7c63257ac838f90d612c34b0722ed804c45
Anri Kopaliani <anrikopa@gmail.com>
2022-08-18 17:37:47 +0400

    added cors

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const cors = require("cors");
 
-app.use(cors());
-
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -30,6 +28,8 @@ io.on("connection", (socket) => {
     console.log(`USER HAS DISCONNECTED ${socket.id}`);
   });
 });
+
+app.use(cors());
 
 server.listen(3001, () => {
   console.log("SERVER IS RUNNING");
